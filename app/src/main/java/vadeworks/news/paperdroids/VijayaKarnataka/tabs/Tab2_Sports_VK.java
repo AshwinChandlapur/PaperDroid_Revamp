e005e3c46075732066519063ebc5dfbd1456af11
AshwinChandlapur <ashwinchandlapur@gmail.com>
2018-02-12 15:59:59 +0530

    VK_Sports ListView Display Ready

--- app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab2_Sports_VK.java
+++ app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab2_Sports_VK.java
@@ -1,5 +1,6 @@
 package vadeworks.news.paperdroids.VijayaKarnataka.tabs;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
@@ -15,7 +21,10 @@ import org.w3c.dom.Document;
 
 import java.util.ArrayList;
 
+import vadeworks.news.paperdroids.Display_news;
+import vadeworks.news.paperdroids.ListView_Adapter;
 import vadeworks.news.paperdroids.News;
+import vadeworks.news.paperdroids.VijayaKarnataka.VijayaKarnataka_Parser;
 import vadeworks.paperdroid.R;
 
 
@@ -59,6 +68,7 @@ public class Tab2_Sports_VK extends Fragment {
                     }catch (Exception e){
                         Log.d("error","error");
                     }
+
                     sports_link_taker = vijayakarnataka_doc.getElementsByClass("dvlstimgs").select("a");
                     Log.d("sports-url","sports-size"+sports_link_taker.size());
                     int i;
@@ -82,15 +92,53 @@ public class Tab2_Sports_VK extends Fragment {
                     }
 
 
+                for(i=0;i<sports_link_taker.size();i++){
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            listView.setAdapter(new ListView_Adapter<News>(context,news) {
+                                @Override
+                                public View getMyView(int i,View view,ViewGroup parent,News news){
+                                    view = getActivity().getLayoutInflater().inflate(R.layout.listview_custom_layout,null);
+                                    TextView news_Headline = (TextView)view.findViewById(R.id.newsHeadlines);
+                                    news_Headline.setText(news.head);
+                                    ImageView news_image = (ImageView)view.findViewById(R.id.newsImage);
+                                    if(!news.imgurl.isEmpty())
+                                    {
+                                        Picasso.with(context).load(news.imgurl).into(news_image);
+                                    }else{
+                                        news_image.setVisibility(View.GONE);
+                                    }
+
+                                    return view;
+                                }
+                            });
 
 
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Intent i = new Intent(getActivity(), Display_news.class);
+                                    VijayaKarnataka_Parser parser = new VijayaKarnataka_Parser();
+                                    News single = parser.Parse_For_Content(news.get(position));
 
+                                    i.putExtra("singleHead",single.head);
+                                    i.putExtra("singleLink",single.link);
+                                    i.putExtra("singleContent",single.content);
+                                    i.putExtra("singleImg",single.imgurl);
+                                    Log.d("Parser single","parser"+news.get(position).head);
+                                    Log.d("Parser single","parser"+news.get(position).link);
+                                    Log.d("Parser single","parser"+single.content);
+                                    Log.d("Parser single","parser"+single.imgurl);
+                                    startActivity(i);
+                                }
+                            });
+                        }
+                    });
 
-
-
-                    //this is of type Document
-
-
+                }
 
 
 
