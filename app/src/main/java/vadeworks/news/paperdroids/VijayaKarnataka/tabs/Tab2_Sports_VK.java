0a324b14581810f0866d66f61f1164806060586c
AshwinChandlapur <ashwinchandlapur@gmail.com>
2018-02-12 14:11:34 +0530

    VK_Sports Parser ready.

--- app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab2_Sports_VK.java
+++ app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab2_Sports_VK.java
@@ -13,6 +13,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 import org.w3c.dom.Document;
 
+import java.util.ArrayList;
+
+import vadeworks.news.paperdroids.News;
 import vadeworks.paperdroid.R;
 
 
@@ -22,7 +25,9 @@ public class Tab2_Sports_VK extends Fragment {
     Context context;
     String vijayakarnataka_url,sports_url;
     org.jsoup.nodes.Document vijayakarnataka_doc ;
-    Elements sports_link_taker;
+    Elements sports_link_taker,sports_link_takers;
+
+    ArrayList<News> news = new ArrayList<News>();
     
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -41,20 +46,45 @@ public class Tab2_Sports_VK extends Fragment {
                     try{
                         vijayakarnataka_doc = Jsoup.connect(vijayakarnataka_url).get();
                     }catch (Exception e){
-                        Log.d("error","erroro");
+                        Log.d("error","error");
                     }
 
                     sports_link_taker = vijayakarnataka_doc.getElementById("nav10738520").select("a");
                     sports_url = sports_link_taker.attr("href");
                     sports_url = vijayakarnataka_url+sports_url;
-                    Log.d("sports-url","sports-url"+sports_url);
+//                    Log.d("sports-url","sports-url"+sports_url);
 
                     try{
                         vijayakarnataka_doc = Jsoup.connect(sports_url).get();
-
                     }catch (Exception e){
+                        Log.d("error","error");
+                    }
+                    sports_link_taker = vijayakarnataka_doc.getElementsByClass("dvlstimgs").select("a");
+                    Log.d("sports-url","sports-size"+sports_link_taker.size());
+                    int i;
+                    for(i=0;i<sports_link_taker.size();i++){
+                        String link =sports_link_taker.get(i).attr("href");
+                        link = vijayakarnataka_url+link;
+                        Log.d("sports-url","sports-link "+link);
+
+
+                        sports_link_takers = vijayakarnataka_doc.getElementsByClass("dvlstimgs").select("a").select("img");
+                        String imgurl = sports_link_takers.get(i).attr("src");
+                        imgurl = vijayakarnataka_url+imgurl;
+                        Log.d("sports-url","sports-image "+imgurl);
+
+
+                        String headline = sports_link_takers.get(i).attr("title");
+                        Log.d("sports-url","sports-headline "+headline);
+
+                        news.add(new News(headline,link,imgurl));
 
                     }
+
+
+
+
+
 
 
 
