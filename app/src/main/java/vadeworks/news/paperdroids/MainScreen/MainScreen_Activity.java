6e126dc341646c709aa6e74ab201ba46bcf66d88
AshwinChandlapur <ashwinchandlapur@gmail.com>
2018-02-17 19:17:40 +0530

    New Loader for all Tabs.

--- app/src/main/java/vadeworks/news/paperdroids/MainScreen/MainScreen_Activity.java
+++ app/src/main/java/vadeworks/news/paperdroids/MainScreen/MainScreen_Activity.java
@@ -1,7 +1,15 @@
 package vadeworks.news.paperdroids.MainScreen;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import vadeworks.paperdroid.R;
 
@@ -11,5 +19,36 @@ public class MainScreen_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen_activity);
+//
+//        Cursor c = getApplication().getContentResolver().query(ContactsContract.Profile.CONTENT_URI, null, null, null, null);
+//        c.moveToFirst();
+////        textView.setText(c.getString(c.getColumnIndex("display_name")));
+//        c.close();
+//
+//
+////        String username = getUsername();
+//        Log.d("Username is","Username is"+c.getColumnIndex("display_name"));
+
+    }
+
+
+    public String getUsername() {
+        AccountManager manager = AccountManager.get(this);
+        Account[] accounts = manager.getAccountsByType("com.google");
+        List<String> possibleEmails = new LinkedList<String>();
+
+        for (Account account : accounts) {
+            // TODO: Check possibleEmail against an email regex or treat
+            // account.name as an email address only for certain account.type values.
+            possibleEmails.add(account.name);
+        }
+
+        if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
+            String email = possibleEmails.get(0);
+            String[] parts = email.split("@");
+            if (parts.length > 1)
+                return parts[0];
+        }
+        return null;
     }
 }
