d08cdd8b4d16dbb7d6473e960ce6c4cfe0924cc1
AshwinChandlapur <ashwinchandlapur@gmail.com>
2018-02-19 20:21:53 +0530

    ThreadStarter Class for VK

--- app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab1_Headlines_VK.java
+++ app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab1_Headlines_VK.java
@@ -23,107 +23,31 @@ import java.util.ArrayList;
 import vadeworks.news.paperdroids.Display_news;
 import vadeworks.news.paperdroids.ListView_Adapter;
 import vadeworks.news.paperdroids.News;
+import vadeworks.news.paperdroids.UdayaVaani.tabs.ThreadStarter_UV;
 import vadeworks.news.paperdroids.VijayaKarnataka.VijayaKarnataka_Parser;
 import vadeworks.paperdroid.R;
 
 public class Tab1_Headlines_VK extends Fragment {
 
-    ListView listView;
     Context context;
-    String tag = "vk_headlines";
-
-    ViewHolder viewHolder;
-    TypingIndicatorView typingView;
-    SwipeRefreshLayout mySwipreRefreshLayout;
-
-    static class ViewHolder {
-        static TextView news_headline;
-        static ImageView news_image;
-    }
-
-    ArrayList<News> news = new ArrayList<News>();
+    View view;
+    String tag = "headlines";
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.vijayakarnataka_tab1_headlines,container,false);
-        init(v);
+        view = inflater.inflate(R.layout.vijayakarnataka_tab1_headlines,container,false);
+        init(view);
+
+        ThreadStarter_VK threadStarter = new ThreadStarter_VK();
+        threadStarter.threadShuruKaro(getActivity(),context,view,tag);
 
 
-
-        //For VijayaKarnataka Main Headlines//
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                VijayaKarnataka_Parser parser = new VijayaKarnataka_Parser();
-                news = parser.parseHeadLines();
-                        if(getActivity() == null)
-                            return;
-                        getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                    listView.setAdapter(new ListView_Adapter<News>(context,news) {
-                                        @Override
-                                        public View getMyView(int i,View view,ViewGroup parent,News news){
-                                            view = layoutinflater(view,news);
-                                            return view;
-                                        }
-                                    });
-                                    typingView.setVisibility(View.GONE);
-                            }
-                        });
-            }
-        }).start();
-
-        listviewOnClick();
-
-
-
-        return v;
+        return view;
     }
 
 
     public void init(View v){
-        listView = (ListView) v.findViewById(R.id.vk_news);
         context = getActivity().getApplicationContext();
-        typingView = (TypingIndicatorView)v.findViewById(R.id.loader);
-
-
-    }
-    public void listviewOnClick(){
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getActivity(), Display_news.class);
-                i.putExtra("singleHead",news.get(position).head);
-                i.putExtra("singleLink",news.get(position).link);
-                i.putExtra("tag","vijayakarnataka");
-                startActivity(i);
-            }
-        });
-    }
-
-
-    public void threadStarter(){
-
-
-    }
-
-    public View layoutinflater(View view,News news){
-        if((view == null)|| (view.getTag() == null))
-        {
-            view = getActivity().getLayoutInflater().inflate(R.layout.listview_custom_layout,null);
-            viewHolder = new ViewHolder();
-        }else{
-            viewHolder = (ViewHolder) view.getTag();
-        }
-        viewHolder.news_headline = (TextView)view.findViewById(R.id.newsHeadlines);
-        viewHolder.news_image = (ImageView)view.findViewById(R.id.newsImage);
-        view.setTag(viewHolder);
-        viewHolder.news_headline.setText(news.head);
-        viewHolder.news_image.setVisibility(View.GONE);
-
-        return view;
     }
 
 }
--- app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab4_Lifestyle_VK.java
+++ app/src/main/java/vadeworks/news/paperdroids/VijayaKarnataka/tabs/Tab4_Lifestyle_VK.java
@@ -36,20 +36,9 @@ import vadeworks.paperdroid.R;
 public class Tab4_Lifestyle_VK extends Fragment {
 
 
-    ListView listView;
     Context context;
-
-    ArrayList<News> news = new ArrayList<News>();
-
-    ViewHolder viewHolder;
-    TypingIndicatorView typingView;
-
-
-    static class ViewHolder {
-        static TextView news_headline;
-        static ImageView news_image;
-    }
-
+    View view;
+    String tag = "lifestyle";
 
     public Tab4_Lifestyle_VK() {
         // Required empty public constructor
@@ -60,86 +49,17 @@ public class Tab4_Lifestyle_VK extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.vijayakarnataka_common_tab, container, false);
-
-        init(v);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        view= inflater.inflate(R.layout.vijayakarnataka_common_tab, container, false);
+        init(view);
+        ThreadStarter_VK threadStarter = new ThreadStarter_VK();
+        threadStarter.threadShuruKaro(getActivity(),context,view,tag);
 
 
-                VijayaKarnataka_Parser parser = new VijayaKarnataka_Parser();
-                news = parser.parseCategory(parser.lifestyle);
-                    // here you check the value of getActivity() and break up if needed
-                    if(getActivity() == null)
-                        return;
-
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                                listView.setAdapter(new ListView_Adapter<News>(context,news) {
-                                    @Override
-                                    public View getMyView(int i,View view,ViewGroup parent,News news){
-                                        view = layoutinflater(view,news);
-                                        return view;
-                                    }
-                                });
-                    typingView.setVisibility(View.GONE);
-                        }
-                    });
-
-
-
-            }
-        }).start();
-        listviewOnClick();
-
-
-
-
-
-        return v;
-    }
-    public void init(View v){
-        listView = (ListView) v.findViewById(R.id.vk_news);
-        context = getActivity().getApplicationContext();
-        typingView = (TypingIndicatorView)v.findViewById(R.id.loader);
-    }
-    public void listviewOnClick(){
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getActivity(), Display_news.class);
-                i.putExtra("singleHead",news.get(position).head);
-                i.putExtra("singleLink",news.get(position).link);
-                i.putExtra("tag","vijayakarnataka");
-                startActivity(i);
-            }
-        });
-    }
-
-    public View layoutinflater(View view,News news){
-        if((view == null)|| (view.getTag() == null))
-        {
-            view = getActivity().getLayoutInflater().inflate(R.layout.listview_custom_layout,null);
-            viewHolder = new ViewHolder();
-        }else{
-            viewHolder = (ViewHolder) view.getTag();
-        }
-        viewHolder.news_headline = (TextView)view.findViewById(R.id.newsHeadlines);
-        viewHolder.news_image = (ImageView)view.findViewById(R.id.newsImage);
-        view.setTag(viewHolder);
-        viewHolder.news_headline.setText(news.head);
-        if(!news.imgurl.isEmpty())
-        {
-            Picasso.with(context).load(news.imgurl).into(viewHolder.news_image);
-        }else{
-            viewHolder.news_image.setVisibility(View.GONE);
-        }
 
         return view;
+    }
+    public void init(View v){
+        context = getActivity().getApplicationContext();
     }
 
 }
