c248b5c52f9a2c9d0ed326738de7d6783ce1e7e9
AshwinChandlapur <ashwinchandlapur@gmail.com>
2018-02-22 19:57:08 +0530

    Alpha Build One. Lint Cleaning.

--- app/src/main/java/vadeworks/news/paperdroids/All_Terms/tabs/Tab2.java
+++ app/src/main/java/vadeworks/news/paperdroids/All_Terms/tabs/Tab2.java
@@ -17,13 +17,13 @@ public class Tab2 extends Fragment {
 
     private final String privacy="Privacy Policy\n" +
             "\n" +
-            "VadeWorks built the News Guru app as an Ad Supported app. This SERVICE is provided by VadeWorks at no cost and is intended for use as is.\n" +
+            "VadeWorks built the News Duniya app as an Ad Supported app. This SERVICE is provided by VadeWorks at no cost and is intended for use as is.\n" +
             "\n" +
             "This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.\n" +
             "\n" +
             "If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.\n" +
             "\n" +
-            "The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at News Guru unless otherwise defined in this Privacy Policy.\n" +
+            "The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at News duniya unless otherwise defined in this Privacy Policy.\n" +
             "\n" +
             "Information Collection and Use\n" +
             "\n" +
